fcdb8d458f4b582cc22e3c2b9b0892bf3319aa87
Siddharth Mishra <siddharth.2023UG2002@iiitranchi.ac.in>
2024-11-14 22:41:19 +0530

    .

--- app/config.py
+++ app/config.py
@@ -3,7 +3,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your_secret_key_here'
     UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')  # Define the upload folder
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB upload limit
 
