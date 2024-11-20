8a65ba2843aae7e1f48a674b0e63011a74fbbc82
Siddharth Mishra <siddharth.2023UG2002@iiitranchi.ac.in>
2024-11-20 16:48:56 +0530

    done

--- app/config.py
+++ app/config.py
@@ -3,7 +3,8 @@
 import os
 
 class Config:
-    UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')  # Define the upload folder
+    UPLOAD_FOLDER = '/path/to/deployment/storage'
+    # UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')  # Define the upload folder local
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB upload limit
 
 os.makedirs(Config.UPLOAD_FOLDER, exist_ok=True)
