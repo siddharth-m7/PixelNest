d646721f8ff9605c3ec2238475fe8684901bcee7
Siddharth Mishra <siddharth.2023UG2002@iiitranchi.ac.in>
2024-11-20 18:26:58 +0530

    .

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@
 import os
 
 class Config:
-    # UPLOAD_FOLDER = '/path/to/deployment/storage'
-    UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')  # Define the upload folder local
+    UPLOAD_FOLDER = '/path/to/deployment/storage'
+    # UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')  # Define the upload folder local
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB upload limit
